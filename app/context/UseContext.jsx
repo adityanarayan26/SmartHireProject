7f7d58557aca6b425f429e380379a9edf64f2e42
adityanarayan26 <adityanarayanraikoti@gmail.com>
2024-12-24 15:58:45 +0530

    changed

--- app/context/UseContext.jsx
+++ app/context/UseContext.jsx
@@ -1,18 +1,27 @@
-"use client"
-import React, { createContext, useContext, useState } from 'react'
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
-const ContextStore = createContext()
+const ContextStore = createContext();
 export const UseContext = () => {
-    return useContext(ContextStore)
-}
+  return useContext(ContextStore);
+};
 
 const ContextProvider = ({ children }) => {
-    const [storeData, setStoreData] = useState(JSON.parse(localStorage.getItem('user')))
-    return (
-        <ContextStore.Provider value={{ storeData, setStoreData }}>
-            {children}
-        </ContextStore.Provider>
-    )
-}
+  const [storeData, setStoreData] = useState(null);
 
-export default ContextProvider
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Initialize state with localStorage data
+      const storedUser = JSON.parse(localStorage.getItem("user"));
+      setStoreData(storedUser);
+    }
+  }, []); // Run once after component mounts
+
+  return (
+    <ContextStore.Provider value={{ storeData, setStoreData }}>
+      {children}
+    </ContextStore.Provider>
+  );
+};
+
+export default ContextProvider;
