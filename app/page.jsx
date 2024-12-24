f40d9c18beb61c476ada344d11b12fdfccb8b799
adityanarayan26 <adityanarayanraikoti@gmail.com>
2024-12-25 00:42:26 +0530

    fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
-const page = () => {
+const Page = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (window.location.pathname !== '/dashboard') {
+      router.push('/dashboard')
+    }
+  }, [router])
+
   return (
-    <div>page</div>
+    <div>Redirecting...</div>
   )
 }
 
-export default page
+export default Page
